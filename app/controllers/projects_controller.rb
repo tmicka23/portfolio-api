b3a1a1ee2191dfde91763830f4fb5f28c297dad5
tmicka23 <mickathp42@gmail.com>
2020-07-22 20:25:37 +0200

    style forms & enable association between projects & tags

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:name, :date, :image, :repository, :link, :client, :content)
+      params.require(:project).permit(:name, :date, :image, :repository, :link, :client, :content, tag_ids: [])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_150902) do
+ActiveRecord::Schema.define(version: 2020_07_22_163436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_07_22_150902) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects_tags", id: false, force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.bigint "tag_id", null: false
+    t.index ["project_id", "tag_id"], name: "index_projects_tags_on_project_id_and_tag_id"
+    t.index ["tag_id", "project_id"], name: "index_projects_tags_on_tag_id_and_project_id"
   end
 
   create_table "tags", force: :cascade do |t|
